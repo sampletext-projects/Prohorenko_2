9c064cdd483c4a0fd0f708e04014a45fdae5bf15
sampletext32 <sampletext32@bk.ru>
2020-05-11 22:50:38 +0300

    Add project

---
+++ Prohorenko_2/main.cpp
@@ -0,0 +1,35 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+double V(double r, double h)
+{
+	//pi * r^2 * h
+	return M_PI * r * r * h;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделала Прохоренко Полина Андреевна студент группы УТН-111\n";
+	cout << "Программа для расчёта объёмов цилиндров\n";
+
+	for (double h = 12; h <= 15; h++)
+	{
+		for (double r = 5; r <= 20; r += 5)
+		{
+			double v = V(r, h);
+			cout << "H: " << setw(2) << h <<
+				" см, R: " << setw(2) << r <<
+				" см, V: " << setw(6) << setprecision(5) << v <<
+				" см^3\n";
+		}
+	}
+
+	system("pause");
+
+	return 0;
+}
